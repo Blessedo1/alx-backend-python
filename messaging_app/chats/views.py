e521a4f4c6e311ff3468ba889d29831b7a43e149
Blessed Adesina <adesinablessed19@gmail.com>
2026-05-01 16:09:23 +0100

    code added

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import User, Conversation, Message
+from .serializers import (
+    UserSerializer,
+    ConversationSerializer,
+    ConversationListSerializer,
+    MessageSerializer,
+    MessageCreateSerializer
+)
+from rest_framework import authentication, permissions
+from rest_framework import action
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing conversations.
+    Users can only access conversations they participate in..
+    """
+    serializer_class = ConversationSerializer
+    def get_queryset(self):
+        """
+        Return only conversations where the current user is a participant.
+        """
+        return Conversation.objects.filter(participants=self.request.user)
+
+class MessageViewset(viewsets.ModelViewSet):
+    """
+    ViewSet for managing messages.
+    Users can only access messages from conversations they participate in.
+    Implements pagination (20 messages per page) and filtering.
+    """
+    serializer_class = MessageSerializer
+    def get_queryset(self):
+        """
+        Return messages from conversations where the current user is a participant.
+        """
+        user_conversations = Conversation.objects.filter(
+            participants=self.request.user
+        )
+        return Message.objects.filter(
+            conversation__in=user_conversations
+        )
